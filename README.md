92d682a35a50ece44f670273f79c958fa88634fd
Dora Do <doradocodes@gmail.com>
2024-09-11 15:58:54 -0700

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# MEDP 33100 - Week 3 Homework
+## Javascript and DOM manipulation
+
+### Instructions 
+1. Fork this repository to your Github account
+2. Clone the repository
+3. Update the `script.js` file to finish building the page:
+- Use the `photos` array to add images to the `#gallery` container
+- Show only 6 photos at a time. When a user clicks the "Load more photos" button, show 6 more photos.
+- When a user clicks the "All" button, show all the photos
+- When a user clicks the "Nature" button, show only the photos related to nature
+- When a user clicks the "City" button, show only the photos related to cities
+- When a user clicks the "Animals" button, show only the photos related to animals.
+4. Push your changes to your forked repository 
+5. Host your page using Github Pages
+5. Add the link to your webpage to this [spreadsheet](https://docs.google.com/spreadsheets/d/1ljYqvvO-DeHI4VusMOgymsxbPTgJBKeNCln1Mt7FOpQ/edit?usp=sharing)
+
+### Deadline
+Due Thursday, September 19, 2024 at 12:00am
---
+++ script.js
@@ -0,0 +1,50 @@
+const photos = [
+    {
+        url: 'https://plus.unsplash.com/premium_photo-1673292293042-cafd9c8a3ab3?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'nature'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1472396961693-142e6e269027?q=80&w=952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'nature'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1482938289607-e9573fc25ebb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'nature'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1509316975850-ff9c5deb0cd9?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'nature'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?q=80&w=944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'city'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'city'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1496568816309-51d7c20e3b21?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'city'
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1494522855154-9297ac14b55f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'city'
+    },
+    {
+        url: 'https://plus.unsplash.com/premium_photo-1669725687221-6fe12c2da6b1?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'animals',
+    },
+    {
+        url: 'https://plus.unsplash.com/premium_photo-1666278379770-440439b08656?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'animals',
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1441057206919-63d19fac2369?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'animals',
+    },
+    {
+        url: 'https://images.unsplash.com/photo-1535268647677-300dbf3d78d1?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        type: 'animals',
+    },
+];
